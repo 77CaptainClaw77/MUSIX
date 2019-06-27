b349ee076d0cdc99a1ab2dc2efe50701496b9232
77CaptainClaw77 <kumar1235813@gmail.com>
2019-06-27 06:12:44 +0530

    more changes

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from flask import redirect
 from werkzeug.utils import secure_filename
 #from sqlalchemy.orm import relationship
 
+is_auth=0
+current_user_logged_id=-1
+
 allowed_music_file_types=['wav','mp3','m4a','wma']
 allowed_image_files_types=['jpeg','jpg','png']
 
@@ -38,9 +41,9 @@ class User(db.Model):
     password=db.Column(db.String(100),nullable=False)
     email=db.Column(db.String(30),nullable=False)
     phone=db.Column(db.String(15),nullable=False)
-    favourites_list=db.Column(db.String(500),nullable=True)# holds ids of users favourite songs
+    favourites=db.Column(db.String(500),nullable=False,default='0')# holds ids of users favourite songs
     visibility=db.Column(db.Integer,default=0,nullable=False)
-    profile_picture=db.Column(db.String(50),default='Default',nullable=False)
+    profile_picture=db.Column(db.String(50),default='Unknown.jpg',nullable=False)
 #--------------------------------------------------------------------------------------------
 
 #Music Databse
@@ -49,7 +52,7 @@ class Music(db.Model):
     __tablename__='MusicInfo'
     song_id=db.Column(db.Integer,primary_key=True)
     song_name=db.Column(db.String(30),nullable=False)
-    song_path=db.Column(db.String(100),nullable=False)
+    song_fname=db.Column(db.String(100),nullable=False)
     artist=db.relationship('Artist',backref='music',uselist=False)
     album=db.relationship('Album',backref='music',uselist=False)
 #--------------------------------------------------------------------------------------------
@@ -59,7 +62,7 @@ class Artist(db.Model):
     __tablename__='ArtistInfo'
     artist_id=db.Column(db.Integer,primary_key=True)
     artist_name=db.Column(db.String(40),nullable=False,default='Unknown')
-    artist_image_path=db.Column(db.String(100),nullable=False,default='Path-To-File')
+    artist_fname=db.Column(db.String(100),nullable=False,default='Unknown.jpg')
     song_id=db.Column(db.Integer,db.ForeignKey(Music.song_id))
 #Album Database
 #--------------------------------------------------------------------------------------------
@@ -67,31 +70,102 @@ class Album(db.Model):
     __tablename__='AlbumInfo'
     album_id=db.Column(db.Integer,primary_key=True)
     album_name=db.Column(db.String(30),nullable=False,default='Unknown')
-    album_cover_image_path=db.Column(db.String(100),nullable=False,default='Path-To-File')
+    album_fname=db.Column(db.String(100),nullable=False,default='Unknown.jpg')
     song_id=db.Column(db.Integer,db.ForeignKey(Music.song_id))    
 #--------------------------------------------------------------------------------------------
-
+#Index Page
+#-------------------------------------------------------------------------------------------
+@app.route('/',methods=['POST','GET'])
+def index_page():
+    global is_auth,current_user_logged_id
+    if request.method=='POST':
+        if is_auth==0:
+            print(str(current_user_logged_id))
+            return redirect(url_for('user_login'))
+        if 'player' in request.form:
+            print('Goes to All music Here with '+str(is_auth)+" and user id is "+str(current_user_logged_id))
+            return redirect(url_for('all_music'))
+        if 'profile' in request.form:
+            return redirect(url_for('profile',u_id=current_user_logged_id))
+    return render_template("index.html")
+    
+#-------------------------------------------------------------------------------------------
 #Login Functionality
 #---------------------------------------------------------------------------------------------
 @app.route('/login/',methods=['POST','GET'])
 def user_login():
+    global is_auth,current_user_logged_id
     if request.method=='POST':
+        if 'signup' in request.form:
+            return redirect(url_for('sign_up'))
         data=User.query.all()
         u_n=request.form['username']
         p=request.form['password']
         #print(data)
         for i in data:
-            if i.username==u_n and check_password_hash(i.password,p):
-                print(i)
-                return redirect(url_for('profile',id=i.user_id))
+            if (i.username==u_n and check_password_hash(i.password,p)):
+                print('was here')
+                current_user_logged_id=i.user_id
+                is_auth=1
+                return redirect(url_for('index_page'))
         return render_template('login.html',data='fail')
     return render_template('login.html',data='no_attempt')
 #------------------------------------------------------------------------------------------------
-
+#Edit Profile
+#------------------------------------------------------------------------------------------------
+@app.route('/edit_profile/<int:u_id>/',methods=['GET','POST'])
+def edit_profile(u_id):
+    global is_auth,current_user_logged_id
+    user_data=User.query.filter_by(user_id=current_user_logged_id).first()
+    if is_auth==0 or current_user_logged_id!=u_id:
+        current_user_logged_id=-1
+        is_auth=0
+        return redirect(url_for('user_login')) 
+    if request.method=='POST':#Making Changes
+        session_profile_edit=db.session
+        old_data=User.query.filter_by(user_id=u_id).first()
+        u_name=request.form['uname']
+        name=request.form['name']
+        pass_old=request.form['password']
+        pass_new=request.form['new_password']
+        ph=request.form['phone']
+        em=request.form['email']
+        prof_pic=request.files['profile_pic']
+        if check_password_hash(pass_old,old_data.password)==False:
+            return render_template('edit_profile.html',data=user_data,error="Password Entered is Incorrect")
+        if  not file_validate(prof_pic,'image'):
+            return render_template('edit_profile.html',data=user_data,error="Improper File Format")
+        old_data.name=name
+        old_data.username=u_name
+        old_data.password=pass_new
+        old_data.phone=ph
+        old_data.email=em
+        if prof_pic!=None:
+            sec_prof_picname=secure_filename(prof_pic.filename)
+            prof_pic.save(os.path.join('static/img/profile',sec_prof_picname))
+            old_data.profile_picture=sec_prof_picname
+        try:
+            session_profile_edit.commit()
+        except:
+            session_profile_edit.rollback()
+            session_profile_edit.flush()
+            return render_template('edit_profile.html',data=user_data,error='Database Error')
+        return redirect(url_for('profile',u_id=current_user_logged_id))
+        # here changes are made and redirected to the profile page
+        #commit changes to DB
+    return render_template('edit_profile.html',data=user_data,error=None)
+#------------------------------------------------------------------------------------------------
 #Profile Page
 #------------------------------------------------------------------------------------------------
-@app.route('/user_profile/<int:id>')
+@app.route('/user_profile/<int:u_id>',methods=['POST','GET'])
 def profile(u_id):
+    global is_auth,current_user_logged_id
+    if is_auth==0:
+        current_user_logged_id=-1
+        is_auth=0
+        return redirect(url_for('user_login'))
+    if request.method=='POST':
+        return redirect(url_for('edit_profile',u_id=u_id))
     try:
         profile_data=User.query.filter_by(user_id=u_id).first()
         return render_template('profile.html',data=profile_data)
@@ -103,12 +177,24 @@ def profile(u_id):
 #------------------------------------------------------------------------------------------------
 @app.route('/signup/',methods=['POST','GET'])
 def sign_up():
+    global is_auth,current_user_logged_id
     if request.method=='POST':
         n=request.form['name']
         u_n=request.form['uname']
         p=request.form['password']
         em=request.form['email']
         ph=request.form['phone']
+        rpw=request.form['rpassword']
+        if rpw!=p:
+            return render_template('signup.html',error="Passwords Do Not Match")
+        query_all=User.query.all()
+        for user in query_all:
+            if u_n==user.username:
+                return render_template('signup.html',error="Username Already Exists")
+            if em==user.email:
+                return  render_template('signup.html',error="Email Already Exists")
+            if ph==user.phone:
+                return render_template('signup.html',error="Phone Number Already Exists")
         session_a=db.session
         insert_data=User(name=n,username=u_n,password=generate_password_hash(p),email=em,phone=ph)
         try:
@@ -118,7 +204,7 @@ def sign_up():
             session_a.rollback()
             session_a.flush()
         return redirect(url_for('user_login'))
-    return render_template('signup.html')
+    return render_template('signup.html',error=None)
 
 #Edit Profile
 # ---------------------------------------------------------------------------------------------
@@ -142,8 +228,12 @@ def file_validate(fname,ftype):
 #----------------------------------------------------------------------------------------------
 @app.route('/upload_music/',methods=['POST','GET'])
 def upload_song():
+    global is_auth,current_user_logged_id
+    if is_auth==0:
+        return redirect(url_for('user_login'))
     if request.method=='POST':
-        song_path=""
+        #song_path=""
+        song_fname=''
         song_name=request.form['song_name']
         artist_name=request.form['artist_name']
         album_name=request.form['album_name']
@@ -154,11 +244,11 @@ def upload_song():
         if s_file and file_validate(s_file.filename,'music'):
             song_fname=secure_filename(s_file.filename)
             s_file.save(os.path.join(app.config['UPLOAD_FOLDER'],song_fname))
-            song_path=os.path.join(app.config['UPLOAD_FOLDER'],song_fname)
+            #song_path=os.path.join(app.config['UPLOAD_FOLDER'],song_fname)
         else:
             return render_template('music_upload.html',data={'error':'Incorrect format of music file uploaded'})
-        album_path=""
-        artist_path=""
+        album_fname=""
+        artist_fname=""
         
         if "artist_image_input" not in request.files:
             artist_path=default_artist_image_path
@@ -167,7 +257,7 @@ def upload_song():
             if artist_img_file and file_validate(artist_img_file.filename,'image'):
                 artist_fname=secure_filename(artist_img_file.filename)
                 artist_img_file.save(os.path.join('./static/img/artist',artist_fname))
-                artist_path=os.path.join('./static/img/artist',artist_fname)
+                #artist_path=os.path.join('./static/img/artist',artist_fname)
             else:
                 return render_template('music_upload.html',data={'error':'Incorrect format of image file uploaded'})
         if "album_image_input" not in request.files:
@@ -177,13 +267,13 @@ def upload_song():
             if album_img_file and file_validate(album_img_file.filename,'image'):
                 album_fname=secure_filename(album_img_file.filename)
                 album_img_file.save(os.path.join('./static/img/album',album_fname))
-                album_path=os.path.join('./static/img/album',album_fname)
+                #album_path=os.path.join('./static/img/album',album_fname)
             else:
                 return render_template('music_upload.html',data={'error':'Incorrect format of image file uploaded'})
         session_new_music=db.session
-        new_song=Music(song_name=song_name,song_path=song_path)
-        new_alb=Album(album_name=album_name,album_cover_image_path=album_path,music=new_song)
-        new_art=Artist(artist_name=artist_name,artist_image_path=artist_path,music=new_song)
+        new_song=Music(song_name=song_name,song_fname=song_fname)
+        new_alb=Album(album_name=album_name,album_fname=album_fname,music=new_song)
+        new_art=Artist(artist_name=artist_name,artist_fname=artist_fname,music=new_song)
         try:
             session_new_music.add(new_song)
             session_new_music.commit()
@@ -202,26 +292,63 @@ def upload_song():
 class music_wrapper:
     def __init__(self,music_data):
         self.song_name=music_data.song_name
-        self.song_path=music_data.song_path
+        self.song_fname=music_data.song_fname
         self.song_id=music_data.song_id
         self.artist_name=music_data.artist.artist_name
         self.album_name=music_data.album.album_name 
-        self.artist_image_path=music_data.artist.artist_image_path
-        self.album_image_path=music_data.album.album_cover_image_path
+        self.artist_fname=music_data.artist.artist_fname
+        self.album_fname=music_data.album.album_fname
 
 #-----------------------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------------------
-@app.route('/play_music/')
-def all_music():
+@app.route('/play_music/',methods=['GET','POST'])
+def all_music():  
+    global is_auth,current_user_logged_id
+    if is_auth==0:
+        return redirect(url_for('user_login'))
+    if request.method=='POST': #To add a song to favourites
+        sess=db.session
+        user_pref=User.query.filter_by(user_id=current_user_logged_id).first()
+        li=[]
+        for i in request.form:
+            li.append(i)
+        print(li)
+        user_pref.favourites=','.join(li)
+        print(user_pref.favourites)
+        try:
+            sess.commit()
+        except:
+            sess.flush()
+            sess.rollback()
+            return 'DB ERROR'
+        return redirect(url_for("index_page"))
     all_music_data=Music.query.all()
     music_list=[]
+    user_data=User.query.filter_by(user_id=current_user_logged_id).first()
+    favourites=[]
+    favourites=user_data.favourites
+    favourites=favourites.split(',')
+    for i in range(len(favourites)):
+        favourites[i]=int(favourites[i])
+    print(favourites)
     for song in all_music_data:
         music_list.append(music_wrapper(song))
-    return render_template('player.html',data=music_list)
+    return render_template('player.html',data=music_list,favourites=favourites)
+#logout functionality
+#-----------------------------------------------------------------------------------------------
+@app.route('/logout/',methods=['POST','GET'])
+def log_out():
+    global is_auth,current_user_logged_id
+    if request.method=='POST':
+        is_auth=0
+        current_user_logged_id=-1
+        return redirect('index_page')
+#-----------------------------------------------------------------------------------------------
+
 
 #Running The Server
 #-----------------------------------------------------------------------------------------------
 if __name__=='__main__':
     db.create_all()
-    app.run(port=8081,debug=True)
+    app.run(port=8085,debug=True)
 #-----------------------------------------------------------------------------------------------
